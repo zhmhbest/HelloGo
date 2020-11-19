e8bdb9f7ddfc52b07777970bfea43b57af0a4c55
zhmhbest <zhmhbest@gmail.com>
2020-11-19 17:03:28 +0800

    update

--- demo/src/demo/variable.go
+++ demo/src/demo/variable.go
@@ -12,3 +12,39 @@ func Define() {
 	fmt.Print("\n")
 	fmt.Print("\n")
 }
+
+func Constant() {
+	const GF = 0.618
+	const PI float64 = 3.1415926535897
+	fmt.Println("GF=", GF)
+	fmt.Println("PI=", PI)
+
+	// 独立计数器常量 iota : const 之后每此自增1
+	const (
+		//i0 //此处报错
+		i1 = "*"
+		i2 = iota
+		i3
+		i4
+		i5 = "#"
+		i6
+		i7
+		i8 = iota
+		i9
+	)
+	fmt.Println("iota1:", i1, i2, i3, i4, i5, i6, i7, i8, i9)
+
+	// 利用iota位移实现位运算
+	const (
+		bin1 = 1 << iota
+		bin2 = 1 << iota
+		bin3 = 1 << iota
+		bin4 = 1 << iota
+		bin5 = 1 << iota
+		bin6 = 1 << iota
+		bin7 = 1 << iota
+		bin8 = 1 << iota
+	)
+	fmt.Println("iota2:", bin1, bin2, bin3, bin4, bin5, bin6, bin7, bin8)
+	fmt.Print("\n")
+}
